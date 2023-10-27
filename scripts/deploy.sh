2ead4151f309d3fb468377f009e406a055f41187
byeolhaha <j35635aa@gmail.com>
2023-10-27 18:22:29 +0900

    Chore : script 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ubuntu/studay
+cd $REPOSITORY
+
+APP_NAME=studay
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
