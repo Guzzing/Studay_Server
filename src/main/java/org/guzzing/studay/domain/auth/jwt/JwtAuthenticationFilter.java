fc4c39c68d221fdd6644ea05bf106c3662a0536e
byeolhaha <j35635aa@gmail.com>
2023-11-01 00:53:43 +0900

    Feat : Jwt Filter 구현

---
+++ src/main/java/org/guzzing/studay/domain/auth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package org.guzzing.studay.domain.auth.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthTokenProvider tokenProvider;
+
+    public JwtAuthenticationFilter(AuthTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        final String AUTHORIZATION_HEADER = request.getHeader("Authorization");
+
+        //Authrization Berear
+        if (AUTHORIZATION_HEADER != null && AUTHORIZATION_HEADER.startsWith("Bearer ")) {
+            String tokenStr = JwtHeaderUtil.getAccessToken(request);
+            AuthToken token = tokenProvider.convertAuthToken(tokenStr);
+
+            if (token.isValidTokenClaims()) {
+                Authentication authentication = tokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        filterChain.doFilter(request, response); //
+
+    }
+
+}
