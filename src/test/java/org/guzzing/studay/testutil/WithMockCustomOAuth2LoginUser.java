52614c5b7d0e9f5371cb12cc7e4a6bdc36fdd661
byeolhaha <j35635aa@gmail.com>
2023-11-01 00:57:47 +0900

    Feat : Mock User  어노테이션으로 구현

---
+++ src/test/java/org/guzzing/studay/testutil/WithMockCustomOAuth2LoginUser.java
@@ -0,0 +1,18 @@
+package org.guzzing.studay.testutil;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomOAuth2LoginUserSecurityContextFactory.class)
+public @interface WithMockCustomOAuth2LoginUser {
+
+    long memberId() default 1L;
+
+    String socialId() default "a";
+
+    String username() default "username";
+
+}
