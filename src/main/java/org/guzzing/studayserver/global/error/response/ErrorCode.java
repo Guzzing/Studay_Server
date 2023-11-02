00720b9edd13c186287e7680e899bca0fe3d460d
byeolhaha <j35635aa@gmail.com>
2023-11-02 15:01:35 +0900

    Feat : ExceptionHandler 구현

---
+++ src/main/java/org/guzzing/studayserver/global/error/response/ErrorCode.java
@@ -0,0 +1,25 @@
+package org.guzzing.studayserver.global.error.response;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    //global
+    INTERNAL_SERVER_ERROR("G001", "Internal Server Error"),
+
+    INVALID_INPUT_VALUE_ERROR("G002", "유효하지 않은 입력값입니다."),
+    INVALID_METHOD_ERROR("G003", "Method Argument가 적절하지 않습니다."),
+    REQUEST_BODY_MISSING_ERROR("G004", "RequestBody에 데이터가 존재하지 않습니다."),
+    REQUEST_PARAM_MISSING_ERROR("G005", "RequestParam에 데이터가 전달되지 않았습니다."),
+    INVALID_TYPE_VALUE_ERROR("G006", "타입이 유효하지 않습니다."),
+    NOT_FOUND_ENTITY("G007", "엔티티를 찾을 수 없습니다.");
+
+    private final String code;
+    private final String message;
+
+    ErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+}
