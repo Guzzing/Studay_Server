b93fce4af04e3a9175692cf07c4fc77186b36def
byeolhaha <j35635aa@gmail.com>
2023-11-02 18:27:54 +0900

    Refactor : 응답 형태  수정

--- src/main/java/org/guzzing/studayserver/domain/academy/controller/AcademyController.java
+++ src/main/java/org/guzzing/studayserver/domain/academy/controller/AcademyController.java
@@ -2,6 +2,8 @@ package org.guzzing.studayserver.domain.academy.controller;
 
 import org.guzzing.studayserver.domain.academy.controller.dto.response.AcademyGetResponse;
 import org.guzzing.studayserver.domain.academy.service.AcademyService;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +24,9 @@ public class AcademyController {
     @GetMapping(
             value = "/{academyId}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AcademyGetResponse> getAcademy( @PathVariable Long academyId) {
-        return ResponseEntity.ok(
-                AcademyGetResponse.of(academyService.getAcademy(academyId))
-        );
+    public ResponseEntity<AcademyGetResponse> getAcademy(@PathVariable Long academyId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(AcademyGetResponse.of(academyService.getAcademy(academyId)));
     }
 
 }
