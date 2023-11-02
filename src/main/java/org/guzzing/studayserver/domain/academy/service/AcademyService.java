49f859b770b2327cf16c79ce1e2ab413a09b8566
byeolhaha <j35635aa@gmail.com>
2023-11-02 14:28:26 +0900

    Feat : 학원 서비스 구현

---
+++ src/main/java/org/guzzing/studayserver/domain/academy/service/AcademyService.java
@@ -0,0 +1,37 @@
+package org.guzzing.studayserver.domain.academy.service;
+
+import org.guzzing.studayserver.domain.academy.model.Academy;
+import org.guzzing.studayserver.domain.academy.model.Lesson;
+import org.guzzing.studayserver.domain.academy.model.ReviewCount;
+import org.guzzing.studayserver.domain.academy.repository.academy.AcademyRepository;
+import org.guzzing.studayserver.domain.academy.repository.lesson.LessonRepository;
+import org.guzzing.studayserver.domain.academy.repository.review.ReviewCountRepository;
+import org.guzzing.studayserver.domain.academy.service.dto.AcademyGetResult;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class AcademyService {
+
+    private final AcademyRepository academyRepository;
+    private final LessonRepository lessonRepository;
+    private final ReviewCountRepository reviewCountRepository;
+
+    public AcademyService(AcademyRepository academyRepository, LessonRepository lessonRepository, ReviewCountRepository reviewCountRepository) {
+        this.academyRepository = academyRepository;
+        this.lessonRepository = lessonRepository;
+        this.reviewCountRepository = reviewCountRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public AcademyGetResult getAcademy(Long academyId) {
+        Academy academy = academyRepository.getById(academyId);
+        List<Lesson> lessons = lessonRepository.findAllByAcademyId(academyId);
+        ReviewCount reviewCount = reviewCountRepository.getByAcademyId(academyId);
+
+        return AcademyGetResult.of(academy, lessons, reviewCount);
+    }
+
+}
