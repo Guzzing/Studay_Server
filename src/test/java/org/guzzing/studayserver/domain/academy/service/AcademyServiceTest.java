1321ff5adcce3aa7010b4ef317764d372db7f679
byeolhaha <j35635aa@gmail.com>
2023-11-03 16:29:49 +0900

    Test: '@Transactional' 추가

--- src/test/java/org/guzzing/studayserver/domain/academy/service/AcademyServiceTest.java
+++ src/test/java/org/guzzing/studayserver/domain/academy/service/AcademyServiceTest.java
@@ -15,9 +15,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @ActiveProfiles({"oauth","dev"})
 @SpringBootTest
 class AcademyServiceTest {
