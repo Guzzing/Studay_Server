b927152939dfbe5c1f4d6dc7e6841a33d2aa4949
SeYoE <onetuks@naver.com>
2023-11-03 14:52:32 +0900

    Refactor: 법정동 데이터 API 분리

--- src/test/java/org/guzzing/studayserver/domain/region/controller/RegionRestControllerTest.java
+++ src/test/java/org/guzzing/studayserver/domain/region/controller/RegionRestControllerTest.java
@@ -12,6 +12,7 @@ import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -57,70 +58,6 @@ class RegionRestControllerTest {
     }
 
     @Test
-    @DisplayName("시도를 파라미터로 요청하면 해당 시도, 시군구, 개수 데이터를 반환한다.")
-    @WithMockCustomOAuth2LoginUser
-    void getSubRegions_Sido_RegionResponse() throws Exception {
-        // Given & When
-        ResultActions perform = mockMvc.perform(get("/regions/beopjungdong")
-                .param("sido", sido)
-                .contentType(APPLICATION_JSON_VALUE)
-                .accept(APPLICATION_JSON_VALUE));
-
-        // Then
-        perform.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON_VALUE))
-                .andExpect(jsonPath("$.targetRegion").value(sido))
-                .andExpect(jsonPath("$.subRegion").isNotEmpty())
-                .andExpect(jsonPath("$.subRegionCount").isNumber())
-                .andDo(document("get-region-beopjungdong-sigungu",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        queryParameters(
-                                parameterWithName("sido").description("시도")
-                        ),
-                        responseFields(
-                                fieldWithPath("targetRegion").type(STRING).description("탐색한 시도명"),
-                                fieldWithPath("subRegion").type(ARRAY).description("탐색한 시군구 조회 결과 리스트"),
-                                fieldWithPath("subRegionCount").type(NUMBER).description("탐색한 시군구 조회 결과 수")
-                        )
-                ));
-    }
-
-    @Test
-    @DisplayName("시도, 시군구를 요청 파라미터로 받아 해당 시도군구의 읍면동 데이터를 응답한다.")
-    @WithMockCustomOAuth2LoginUser
-    void getSubRegions_SidoAndSigungu_RegionResponse() throws Exception {
-        // Given & When
-        ResultActions perform = mockMvc.perform(get("/regions/beopjungdong")
-                .param("sido", sido)
-                .param("sigungu", sigungu)
-                .contentType(APPLICATION_JSON_VALUE)
-                .accept(APPLICATION_JSON_VALUE));
-
-        // Then
-        perform.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON_VALUE))
-                .andExpect(jsonPath("$.targetRegion").value(sido + " " + sigungu))
-                .andExpect(jsonPath("$.subRegion").isNotEmpty())
-                .andExpect(jsonPath("$.subRegionCount").isNumber())
-                .andDo(document("get-region-beopjungdong-upmyeondong",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        queryParameters(
-                                parameterWithName("sido").description("시도"),
-                                parameterWithName("sigungu").description("시군구")
-                        ),
-                        responseFields(
-                                fieldWithPath("targetRegion").type(STRING).description("탐색한 시도군구명"),
-                                fieldWithPath("subRegion").type(ARRAY).description("탐색한 읍면동 조회 결과 리스트"),
-                                fieldWithPath("subRegionCount").type(NUMBER).description("탐색한 읍면동 조회 결과 수")
-                        )
-                ));
-    }
-
-    @Test
     @DisplayName("아무런 파라미터 없이 요청하면 조회 가능한 시도 데이터를 반환한다.")
     @WithMockCustomOAuth2LoginUser
     void getSubRegions_None_RegionResponse() throws Exception {
@@ -143,6 +80,67 @@ class RegionRestControllerTest {
                                 fieldWithPath("targetRegion").type(STRING).description("전국"),
                                 fieldWithPath("subRegion").type(ARRAY).description("탐색 가능한 시도 조회 결과 리스트"),
                                 fieldWithPath("subRegionCount").type(NUMBER).description("탐색 가능한 시도 조회 결과 수")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("시도를 파라미터로 요청하면 해당 시도, 시군구, 개수 데이터를 반환한다.")
+    @WithMockCustomOAuth2LoginUser
+    void getSubRegions_Sido_RegionResponse() throws Exception {
+        // Given & When
+        ResultActions perform = mockMvc.perform(get("/regions/beopjungdong/{sido}", sido)
+                .contentType(APPLICATION_JSON_VALUE)
+                .accept(APPLICATION_JSON_VALUE));
+
+        // Then
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.targetRegion").value(sido))
+                .andExpect(jsonPath("$.subRegion").isNotEmpty())
+                .andExpect(jsonPath("$.subRegionCount").isNumber())
+                .andDo(document("get-region-beopjungdong-sigungu",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("sido").description("시도")
+                        ),
+                        responseFields(
+                                fieldWithPath("targetRegion").type(STRING).description("탐색한 시도명"),
+                                fieldWithPath("subRegion").type(ARRAY).description("탐색한 시군구 조회 결과 리스트"),
+                                fieldWithPath("subRegionCount").type(NUMBER).description("탐색한 시군구 조회 결과 수")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("시도, 시군구를 요청 파라미터로 받아 해당 시도군구의 읍면동 데이터를 응답한다.")
+    @WithMockCustomOAuth2LoginUser
+    void getSubRegions_SidoAndSigungu_RegionResponse() throws Exception {
+        // Given & When
+        ResultActions perform = mockMvc.perform(get("/regions/beopjungdong/{sido}/{sigungu}", sido, sigungu)
+                .contentType(APPLICATION_JSON_VALUE)
+                .accept(APPLICATION_JSON_VALUE));
+
+        // Then
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.targetRegion").value(sido + " " + sigungu))
+                .andExpect(jsonPath("$.subRegion").isNotEmpty())
+                .andExpect(jsonPath("$.subRegionCount").isNumber())
+                .andDo(document("get-region-beopjungdong-upmyeondong",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("sido").description("시도"),
+                                parameterWithName("sigungu").description("시군구")
+                        ),
+                        responseFields(
+                                fieldWithPath("targetRegion").type(STRING).description("탐색한 시도군구명"),
+                                fieldWithPath("subRegion").type(ARRAY).description("탐색한 읍면동 조회 결과 리스트"),
+                                fieldWithPath("subRegionCount").type(NUMBER).description("탐색한 읍면동 조회 결과 수")
                         )
                 ));
     }
