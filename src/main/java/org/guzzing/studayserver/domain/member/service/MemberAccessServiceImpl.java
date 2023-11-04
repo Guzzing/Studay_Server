2a2ba5e2c6faa3901be60b371b4aba3f8431416b
SeYoE <onetuks@naver.com>
2023-11-05 00:52:04 +0900

    Feat: 좋아요, 리뷰 도메인과 멤버 도메인 인터페이스 구현

--- src/main/java/org/guzzing/studayserver/domain/member/service/MemberAccessServiceImpl.java
+++ src/main/java/org/guzzing/studayserver/domain/member/service/MemberAccessServiceImpl.java
@@ -1,13 +1,22 @@
 package org.guzzing.studayserver.domain.member.service;
 
+import org.guzzing.studayserver.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class MemberAccessServiceImpl implements
         MemberAccessService {
 
+    private final MemberRepository memberRepository;
+
+    public MemberAccessServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public boolean existsMember(Long memberId) {
-        return false;
+        return memberRepository.existsById(memberId);
     }
 }
