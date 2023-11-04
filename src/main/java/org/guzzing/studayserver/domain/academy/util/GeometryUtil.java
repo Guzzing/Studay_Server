a0918fce76a462cec52b19ec7ed6adb75e888426
byeolhaha <j35635aa@gmail.com>
2023-11-05 01:27:38 +0900

    Feat : 하버사인 공식을 이용하여 중심 위치 기반  대각선 구하는 로직 구현

---
+++ src/main/java/org/guzzing/studayserver/domain/academy/util/GeometryUtil.java
@@ -0,0 +1,58 @@
+package org.guzzing.studayserver.domain.academy.util;
+
+import org.guzzing.studayserver.domain.academy.model.vo.Location;
+
+public class GeometryUtil {
+
+    private static final Double EARTH_RADIUS = 6371.01;
+
+    private static final String LINESTRING_SQL = "'LINESTRING(%f %f, %f %f)'";
+
+    public static Location calculateLocationWithinRadiusInDirection(Double baseLatitude,
+                                                                    Double baseLongitude,
+                                                                    Double bearing,
+                                                                    Double distance) {
+        Double radianLatitude = toRadian(baseLatitude);
+        Double radianLongitude = toRadian(baseLongitude);
+        Double radianAngle = toRadian(bearing);
+
+        Double distanceRadius = distance / EARTH_RADIUS;
+
+        Double latitude = Math.asin(sin(radianLatitude) * cos(distanceRadius) +
+                cos(radianLatitude) * sin(distanceRadius) * cos(radianAngle));
+        Double longitude = radianLongitude + Math.atan2(sin(radianAngle) * sin(distanceRadius) *
+                cos(radianLatitude), cos(distanceRadius) - sin(radianLatitude) * sin(latitude));
+        longitude = normalizeLongitude(longitude);
+
+        return Location.of(toDegree(latitude), toDegree(longitude));
+    }
+
+    private static Double toRadian(Double coordinate) {
+        return coordinate * Math.PI / 180.0;
+    }
+
+    private static Double toDegree(Double coordinate) {
+        return coordinate * 180.0 / Math.PI;
+    }
+
+    private static Double sin(Double coordinate) {
+        return Math.sin(coordinate);
+    }
+
+    private static Double cos(Double coordinate) {
+        return Math.cos(coordinate);
+    }
+
+    private static Double normalizeLongitude(Double longitude) {
+        return (longitude + 540) % 360 - 180;
+    }
+
+    public static String makeDiagonalByLineString(Location northEast, Location southWest) {
+        return String.format(
+                LINESTRING_SQL,
+                northEast.getLatitude(), northEast.getLongitude(), southWest.getLatitude(), southWest.getLongitude()
+        );
+
+    }
+
+}
