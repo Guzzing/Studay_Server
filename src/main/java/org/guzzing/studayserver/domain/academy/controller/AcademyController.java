cba1fe2dc10bd1035d89fb3d937ea41ed0ea90a5
byeolhaha <j35635aa@gmail.com>
2023-11-05 18:42:35 +0900

    Feat : 학원 이름 자동완성 목록 컨트롤러 구현

--- src/main/java/org/guzzing/studayserver/domain/academy/controller/AcademyController.java
+++ src/main/java/org/guzzing/studayserver/domain/academy/controller/AcademyController.java
@@ -1,13 +1,18 @@
 package org.guzzing.studayserver.domain.academy.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.guzzing.studayserver.domain.academy.controller.dto.request.AcademiesByLocationRequest;
+import org.guzzing.studayserver.domain.academy.controller.dto.request.AcademiesByNameRequest;
 import org.guzzing.studayserver.domain.academy.controller.dto.response.AcademiesByLocationResponses;
+import org.guzzing.studayserver.domain.academy.controller.dto.response.AcademiesByNameResponses;
 import org.guzzing.studayserver.domain.academy.controller.dto.response.AcademyGetResponse;
 import org.guzzing.studayserver.domain.academy.service.AcademyService;
 
 import org.guzzing.studayserver.domain.academy.service.dto.param.AcademiesByLocationParam;
+import org.guzzing.studayserver.domain.academy.service.dto.param.AcademiesByNameParam;
 import org.guzzing.studayserver.domain.academy.service.dto.result.AcademiesByLocationResults;
+import org.guzzing.studayserver.domain.academy.service.dto.result.AcademiesByNameResults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -34,11 +39,20 @@ public class AcademyController {
     @GetMapping(
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AcademiesByLocationResponses> findByLocation(@ModelAttribute @Valid AcademiesByLocationRequest request)  {
-        AcademiesByLocationParam academiesByLocationParam = AcademiesByLocationRequest.to(request);
-        AcademiesByLocationResults academiesByLocation = academyService.findAcademiesByLocation(academiesByLocationParam);
+        AcademiesByLocationResults academiesByLocation =
+                academyService.findAcademiesByLocation(AcademiesByLocationRequest.to(request));
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(AcademiesByLocationResponses.from(academiesByLocation));
+    }
+
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AcademiesByNameResponses> findByName(@ModelAttribute @Valid AcademiesByNameRequest request)  {
+        AcademiesByNameResults academiesByNameResults = academyService.findAcademiesByName(AcademiesByNameRequest.to(request));
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(AcademiesByNameResponses.from(academiesByNameResults));
     }
 
 }
---
+++ src/main/java/org/guzzing/studayserver/domain/academy/controller/dto/response/AcademiesByNameResponse.java
@@ -0,0 +1,17 @@
+package org.guzzing.studayserver.domain.academy.controller.dto.response;
+
+import org.guzzing.studayserver.domain.academy.service.dto.result.AcademiesByNameResult;
+
+public record AcademiesByNameResponse(
+        Long academyId,
+        String academyName,
+        String address
+) {
+    public static AcademiesByNameResponse from(AcademiesByNameResult academiesByNameResult) {
+        return new AcademiesByNameResponse(
+                academiesByNameResult.academyId(),
+                academiesByNameResult.academyName(),
+                academiesByNameResult.address()
+        );
+    }
+}
