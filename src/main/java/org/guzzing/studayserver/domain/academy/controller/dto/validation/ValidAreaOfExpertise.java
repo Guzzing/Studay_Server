1b89efbf2a0069005b694d8916caf577debbf944
byeolhaha <j35635aa@gmail.com>
2023-11-08 18:44:04 +0900

    Feat: 커스텀 검증 어노테이션 구현

---
+++ src/main/java/org/guzzing/studayserver/domain/academy/controller/dto/validation/ValidAreaOfExpertise.java
@@ -0,0 +1,18 @@
+package org.guzzing.studayserver.domain.academy.controller.dto.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidAreaOfExpertiseValidator.class)
+public @interface ValidAreaOfExpertise {
+    String message() default "유효하지 않은 학원 분야입니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
