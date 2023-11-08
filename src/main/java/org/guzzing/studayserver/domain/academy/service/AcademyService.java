aae33ae29b0ce16447fb1a9f813279ebcb66982d
byeolhaha <j35635aa@gmail.com>
2023-11-08 16:05:16 +0900

    Feat: 학원 검색 필터 기능 서비스 구현

--- src/main/java/org/guzzing/studayserver/domain/academy/service/AcademyService.java
+++ src/main/java/org/guzzing/studayserver/domain/academy/service/AcademyService.java
@@ -5,13 +5,16 @@ import org.guzzing.studayserver.domain.academy.model.Academy;
 import org.guzzing.studayserver.domain.academy.model.Lesson;
 import org.guzzing.studayserver.domain.academy.model.ReviewCount;
 import org.guzzing.studayserver.domain.academy.model.vo.Location;
+
 import org.guzzing.studayserver.domain.academy.repository.academy.AcademyRepository;
 import org.guzzing.studayserver.domain.academy.repository.lesson.LessonRepository;
 import org.guzzing.studayserver.domain.academy.repository.review.ReviewCountRepository;
 import org.guzzing.studayserver.domain.academy.service.dto.param.AcademiesByLocationParam;
 import org.guzzing.studayserver.domain.academy.service.dto.param.AcademiesByNameParam;
+import org.guzzing.studayserver.domain.academy.service.dto.param.AcademyFilterParam;
 import org.guzzing.studayserver.domain.academy.service.dto.result.*;
 import org.guzzing.studayserver.domain.academy.util.GeometryUtil;
+import org.guzzing.studayserver.domain.academy.util.SqlFormatter;
 import org.guzzing.studayserver.domain.academy.util.model.Direction;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -20,7 +23,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AcademyService {
 
-    private static final Double DISTANCE = 5.0;
+    private static final Double DISTANCE = 2.0;
+
     private static final int PAGE_SIZE = 5;
 
     private final AcademyRepository academyRepository;
@@ -45,23 +49,19 @@ public class AcademyService {
         return AcademyGetResult.from(academy, lessons, reviewCount);
     }
 
-    @Transactional(readOnly = true)
-    public AcademiesByLocationResults findAcademiesByLocation(AcademiesByLocationParam param) {
-        Location northEast = GeometryUtil.calculateLocationWithinRadiusInDirection(
-                param.baseLatitude(),
-                param.baseLongitude(),
-                Direction.NORTHEAST.getBearing(),
-                DISTANCE);
-        Location southWest = GeometryUtil.calculateLocationWithinRadiusInDirection(
-                param.baseLatitude(),
-                param.baseLongitude(),
-                Direction.SOUTHWEST.getBearing(),
-                DISTANCE);
-        String diagonal = GeometryUtil.makeDiagonalByLineString(northEast, southWest);
 
-        return AcademiesByLocationResults.to(
-                academyRepository.findAcademiesByLocation(diagonal)
-        );
+    public AcademiesByLocationResults findAcademiesByLocation(AcademiesByLocationParam param) {
+        Location northEast = calculateLocationWithinRadiusInDirection(
+                param.baseLatitude(),
+                param.baseLongitude(),
+                Direction.NORTHEAST);
+        Location southWest = calculateLocationWithinRadiusInDirection(
+                param.baseLatitude(),
+                param.baseLongitude(),
+                Direction.SOUTHWEST);
+        String diagonal = SqlFormatter.makeDiagonalByLineString(northEast, southWest);
+
+        return AcademiesByLocationResults.to(academyRepository.findAcademiesByLocation(diagonal));
     }
 
     @Transactional(readOnly = true)
@@ -71,6 +71,25 @@ public class AcademyService {
         return AcademiesByNameResults.to(
                 academyRepository.findAcademiesByName(param.academyName(), requestPageAble)
         );
+    }
+
+    @Transactional(readOnly = true)
+    public AcademyFilterResults filterAcademies(AcademyFilterParam param) {
+        Location northEast = calculateLocationWithinRadiusInDirection(
+                param.baseLatitude(),
+                param.baseLongitude(),
+                Direction.NORTHEAST);
+        Location southWest = calculateLocationWithinRadiusInDirection(
+                param.baseLatitude(),
+                param.baseLongitude(),
+                Direction.SOUTHWEST);
+        String diagonal = SqlFormatter.makeDiagonalByLineString(northEast, southWest);
+
+        return AcademyFilterResults.from(academyRepository.filterAcademies(AcademyFilterParam.to(param, diagonal)));
+    }
+
+    private Location calculateLocationWithinRadiusInDirection(double latitude, double longitude, Direction direction) {
+        return GeometryUtil.calculateLocationWithinRadiusInDirection(latitude, longitude, direction.getBearing(), DISTANCE);
     }
 
 }
