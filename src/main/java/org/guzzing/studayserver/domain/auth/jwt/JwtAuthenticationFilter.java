ea034a2f3edc0b127cbd2cfed2d850bafa505330
byeolhaha <j35635aa@gmail.com>
2023-11-11 02:09:15 +0900

    Feat : jwt 필터에 로그아웃 여부 확인하는 로직 구현

--- src/main/java/org/guzzing/studayserver/domain/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/guzzing/studayserver/domain/auth/jwt/JwtAuthenticationFilter.java
@@ -4,31 +4,39 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
+import org.guzzing.studayserver.domain.auth.service.AuthService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final AuthTokenProvider tokenProvider;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer ";
 
-    public JwtAuthenticationFilter(AuthTokenProvider tokenProvider) {
+    private final AuthTokenProvider tokenProvider;
+    private final AuthService authService;
+
+    public JwtAuthenticationFilter(AuthTokenProvider tokenProvider, AuthService authService) {
         this.tokenProvider = tokenProvider;
+        this.authService = authService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
 
-        final String AUTHORIZATION_HEADER = request.getHeader("Authorization");
+        final String AUTHORIZATION_HEADER = request.getHeader(HEADER_AUTHORIZATION);
 
-        if (AUTHORIZATION_HEADER != null && AUTHORIZATION_HEADER.startsWith("Bearer ")) {
+        if (AUTHORIZATION_HEADER != null && AUTHORIZATION_HEADER.startsWith(TOKEN_PREFIX)) {
             String tokenStr = JwtHeaderUtil.getAccessToken(request);
             AuthToken token = tokenProvider.convertAuthToken(tokenStr);
 
-            if (token.isValidTokenClaims()) {
+            if (!isLogout(token.getToken()) && token.isValidTokenClaims()) {
                 Authentication authentication = tokenProvider.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
@@ -36,7 +44,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
+
         filterChain.doFilter(request, response);
+    }
+
+    private boolean isLogout(String accessToken) {
+        return authService.isLogout(accessToken);
     }
 
 }
