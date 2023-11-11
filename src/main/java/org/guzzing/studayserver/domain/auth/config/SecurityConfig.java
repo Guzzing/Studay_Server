895767ac30b837b27d5391f3dab1bed548c8bc79
byeolhaha <j35635aa@gmail.com>
2023-11-11 14:49:35 +0900

    Style : 불필요한 개행 삭제

--- src/main/java/org/guzzing/studayserver/domain/auth/config/SecurityConfig.java
+++ src/main/java/org/guzzing/studayserver/domain/auth/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package org.guzzing.studayserver.domain.auth.config;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import org.guzzing.studayserver.domain.auth.jwt.AuthTokenProvider;
