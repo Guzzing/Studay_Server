f2ee2241ea8898ab868f531df8bdb0299f7779a3
byeolhaha <j35635aa@gmail.com>
2023-11-11 13:03:31 +0900

    Refactor : cors origin 전체로 설정

--- src/main/java/org/guzzing/studayserver/domain/auth/config/SecurityConfig.java
+++ src/main/java/org/guzzing/studayserver/domain/auth/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
