f44864332eda2a9d5d1f92fcc4db306970e396b9
SeYoE <onetuks@naver.com>
2023-11-14 18:09:27 +0900

    Refactor: QueryDSL 적용

--- src/main/java/org/guzzing/studayserver/domain/dashboard/repository/DashboardJpaRepository.java
+++ src/main/java/org/guzzing/studayserver/domain/dashboard/repository/DashboardJpaRepository.java
@@ -5,9 +5,7 @@ import org.guzzing.studayserver.domain.dashboard.model.Dashboard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface DashboardJpaRepository extends JpaRepository<Dashboard, Long>, DashboardRepository {
-
-    @Query("select d from Dashboard d where d.childId = :childId and d.isActive = true")
-    List<Dashboard> findActiveOnlyByChildId(final Long childId);
+public interface DashboardJpaRepository extends
+        JpaRepository<Dashboard, Long>, DashboardRepository, DashboardQuerydslRepository {
 
 }
---
+++ src/main/java/org/guzzing/studayserver/global/config/QueryDslConfig.java
@@ -0,0 +1,17 @@
+package org.guzzing.studayserver.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(final EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
