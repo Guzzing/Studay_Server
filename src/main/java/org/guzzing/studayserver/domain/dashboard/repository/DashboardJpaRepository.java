e4be43373847eaa6c9fd4af8ef79ed57cc013350
SeYoE <onetuks@naver.com>
2023-11-14 16:32:08 +0900

    Refactor: Repository 상속 구조 변경

--- src/main/java/org/guzzing/studayserver/domain/dashboard/repository/DashboardJpaRepository.java
+++ src/main/java/org/guzzing/studayserver/domain/dashboard/repository/DashboardJpaRepository.java
@@ -1,8 +1,13 @@
 package org.guzzing.studayserver.domain.dashboard.repository;
 
+import java.util.List;
 import org.guzzing.studayserver.domain.dashboard.model.Dashboard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface DashboardJpaRepository extends JpaRepository<Dashboard, Long> {
+public interface DashboardJpaRepository extends JpaRepository<Dashboard, Long>, DashboardRepository {
+
+    @Query("select d from Dashboard d where d.childId = :childId and d.isActive = true")
+    List<Dashboard> findActiveOnlyByChildId(final Long childId);
 
 }
--- src/main/java/org/guzzing/studayserver/domain/dashboard/repository/DashboardRepository.java
+++ src/main/java/org/guzzing/studayserver/domain/dashboard/repository/DashboardRepository.java
@@ -1,14 +1,19 @@
 package org.guzzing.studayserver.domain.dashboard.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.guzzing.studayserver.domain.dashboard.model.Dashboard;
-import org.springframework.data.jpa.repository.Query;
 
-public interface DashboardRepository extends DashboardJpaRepository {
+public interface DashboardRepository {
 
-    @Query("select d from Dashboard d where d.childId = :childId and d.isActive = true")
+    Dashboard save(final Dashboard dashboard);
+
+    Optional<Dashboard> findById(final Long dashboardId);
+
     List<Dashboard> findActiveOnlyByChildId(final Long childId);
 
     List<Dashboard> findAllByChildId(final Long childId);
+
+    List<Dashboard> findAll();
 
 }
