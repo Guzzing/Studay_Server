f9c1dab9659231579b34aa0bfc7067b327a13c05
byeolhaha <j35635aa@gmail.com>
2023-11-16 16:28:26 +0900

    Feat : 반복 여부 전략 패턴 활용하여 호출

---
+++ src/main/java/org/guzzing/studayserver/domain/acdademycalendar/service/PeriodicStrategy.java
@@ -0,0 +1,30 @@
+package org.guzzing.studayserver.domain.acdademycalendar.service;
+
+import org.guzzing.studayserver.domain.acdademycalendar.model.Periodicity;
+import org.guzzing.studayserver.domain.acdademycalendar.service.dto.RepeatPeriod;
+import org.guzzing.studayserver.domain.acdademycalendar.service.scheduler.*;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class PeriodicStrategy {
+
+    private Map<Periodicity, AcademyScheduleMaker> periodicStrategies = new HashMap<>();
+
+    public PeriodicStrategy() {
+        periodicStrategies.put(Periodicity.DAILY,new ScheduleDailyMaker());
+        periodicStrategies.put(Periodicity.MONTHLY,new ScheduleMonthMaker());
+        periodicStrategies.put(Periodicity.WEEKLY,new ScheduleWeekMaker());
+        periodicStrategies.put(Periodicity.YEARLY,new ScheduleYearlyMaker());
+    }
+
+    public List<LocalDate> createSchedules(RepeatPeriod repeatPeriod) {
+        return periodicStrategies.get(repeatPeriod.periodicity())
+                .generateSchedules(repeatPeriod);
+    }
+
+}
