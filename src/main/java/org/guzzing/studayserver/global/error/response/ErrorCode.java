56debb334cb9f0e79adc76b4a242553b89cd7183
byeolhaha <j35635aa@gmail.com>
2023-11-16 17:54:54 +0900

    Feat : 중복된 학원 스케줄 커스컴 예외 구현

--- src/main/java/org/guzzing/studayserver/global/error/response/ErrorCode.java
+++ src/main/java/org/guzzing/studayserver/global/error/response/ErrorCode.java
@@ -25,8 +25,10 @@ public enum ErrorCode {
     EXPIRED_REFRESH_TOKEN("L002", "리프레쉬 토큰도 만료되어 다시 로그인을 요청합니다."),
     UNAUTHORIZED_TOKEN("L003", "인증되지 않은 토큰입니다."),
     OAUTH_CLIENT_SERVER_ERROR("L004", "oauth 클라이언트 서버 에러입니다."),
-    IS_LOGOUT_TOKEN("L005", "이미 로그아웃한 토큰입니다.");
+    IS_LOGOUT_TOKEN("L005", "이미 로그아웃한 토큰입니다."),
 
+    //academy-schedules
+    DATE_TIME_OVERLAP_ERROR("S001","중복된 일정은 등록될 수 없습니다.");
 
     private final String code;
     private final String message;
