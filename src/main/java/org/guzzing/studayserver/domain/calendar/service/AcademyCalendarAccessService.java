a9d36b7dffe0d7f734cfd1b7d2af881792fb5ed8
byeolhaha <j35635aa@gmail.com>
2023-11-17 21:22:44 +0900

    Feat : 대시보드에서 접근하는 학원 스케줄 삭제 AccessService 구현

---
+++ src/main/java/org/guzzing/studayserver/domain/calendar/service/AcademyCalendarAccessService.java
@@ -0,0 +1,9 @@
+package org.guzzing.studayserver.domain.calendar.service;
+
+import org.guzzing.studayserver.domain.calendar.service.dto.param.AcademyCalendarDeleteParam;
+
+public interface AcademyCalendarAccessService {
+
+    void deleteSchedule(AcademyCalendarDeleteParam academyCalendarDeleteParam);
+    
+}
