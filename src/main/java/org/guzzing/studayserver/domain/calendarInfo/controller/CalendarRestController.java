537b6414277dbf6f72a229ee704df1e0859d3748
wonu606 <wonu606@gmail.com>
2023-11-22 16:16:10 +0900

    fix: "calendar/mark" 파라미터를 받지 못하는 버그 수정

--- src/main/java/org/guzzing/studayserver/domain/calendarInfo/controller/CalendarRestController.java
+++ src/main/java/org/guzzing/studayserver/domain/calendarInfo/controller/CalendarRestController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +37,7 @@ public class CalendarRestController {
     )
     public ResponseEntity<CalendarYearMonthMarkResponse> findYearMonthMark(
             @MemberId Long memberId,
-            @RequestParam @Valid CalendarYearMonthMarkRequest request
+            @ModelAttribute @Valid CalendarYearMonthMarkRequest request
     ) {
 
         CalendarYearMonthMarkResult result = calendarFacade.findYearMonthMark(request.toParam(memberId));
