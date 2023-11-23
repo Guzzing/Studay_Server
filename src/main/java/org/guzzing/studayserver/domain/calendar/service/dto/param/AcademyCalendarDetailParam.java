ba986f9045f5e637a8c32bb868be6b83688c99f0
byeolhaha <j35635aa@gmail.com>
2023-11-23 17:05:06 +0900

    Feat : 스케줄 상세보기 관련 서비스 구현

---
+++ src/main/java/org/guzzing/studayserver/domain/calendar/service/dto/param/AcademyCalendarDetailParam.java
@@ -0,0 +1,14 @@
+package org.guzzing.studayserver.domain.calendar.service.dto.param;
+
+import java.util.List;
+
+public record AcademyCalendarDetailParam(
+        List<ChildrenSchedule> childrenInfos
+) {
+    public record ChildrenSchedule(
+            Long childId,
+            Long scheduleId
+    ) {
+    }
+
+}
---
+++ src/main/java/org/guzzing/studayserver/domain/calendar/service/dto/result/AcademyCalendarDetailResults.java
@@ -0,0 +1,41 @@
+package org.guzzing.studayserver.domain.calendar.service.dto.result;
+
+import org.guzzing.studayserver.domain.calendar.repository.dto.AcademyCalenderDetailInfo;
+
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public record AcademyCalendarDetailResults(
+        Map<Long, AcademyCalendarDetailResult> academyCalendarDetailResults
+
+) {
+    public static AcademyCalendarDetailResults from(List<AcademyCalenderDetailInfo> academyCalenderDetailInfos) {
+        Map<Long, AcademyCalendarDetailResult> academyCalendarDetailResults = new HashMap<>();
+        academyCalenderDetailInfos.
+                forEach(academyCalenderDetailInfo ->
+                        academyCalendarDetailResults.put(academyCalenderDetailInfo.getChildId()
+                                , AcademyCalendarDetailResult.from(academyCalenderDetailInfo))
+
+                );
+        return new AcademyCalendarDetailResults(academyCalendarDetailResults);
+    }
+
+    public record AcademyCalendarDetailResult(
+            Long dashboardId,
+            LocalTime lessonStartTime,
+            LocalTime lessonEndTime,
+            String memo
+    ) {
+        public static AcademyCalendarDetailResult from(AcademyCalenderDetailInfo academyCalenderDetailInfo) {
+            return new AcademyCalendarDetailResult(
+                    academyCalenderDetailInfo.getDashboardId(),
+                    academyCalenderDetailInfo.getLessonStartTime(),
+                    academyCalenderDetailInfo.getLessonEndTime(),
+                    academyCalenderDetailInfo.getMemo()
+            );
+        }
+    }
+
+}
