6270f39d06433c7fcb1a751c5c4f9f91a53e2f12
byeolhaha <j35635aa@gmail.com>
2023-11-27 03:17:39 +0900

    Feat : 학원 필터 매핑 테이블 레파지토리 구현

---
+++ src/main/java/org/guzzing/studayserver/domain/academy/repository/academy/AcademyCategoryRepository.java
@@ -0,0 +1,9 @@
+package org.guzzing.studayserver.domain.academy.repository.academy;
+
+import org.guzzing.studayserver.domain.academy.model.AcademyCategory;
+
+public interface AcademyCategoryRepository {
+
+    AcademyCategory save(AcademyCategory academyCategory);
+
+}
