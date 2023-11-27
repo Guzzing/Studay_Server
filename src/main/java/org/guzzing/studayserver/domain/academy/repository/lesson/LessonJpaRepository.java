bc592d3b17c112ad32a649936c2e0051e3f01e44
byeolhaha <j35635aa@gmail.com>
2023-11-27 15:04:16 +0900

    Refactor : Lesson 관련 쿼리들 수정

--- src/main/java/org/guzzing/studayserver/domain/academy/repository/lesson/LessonJpaRepository.java
+++ src/main/java/org/guzzing/studayserver/domain/academy/repository/lesson/LessonJpaRepository.java
@@ -11,13 +11,14 @@ import org.springframework.data.repository.query.Param;
 
 public interface LessonJpaRepository extends JpaRepository<Lesson, Long>, LessonRepository {
 
-    List<Lesson> findAllByAcademyId(Long academyId);
+    @Query("select l from Lesson as l join fetch l.academy as ac where l.academy.id =:academyId")
+    List<Lesson> findAllByAcademyId(@Param("academyId") Long academyId);
 
     @Query("select l from Lesson as l join fetch l.academy as ac where l.id = :lessonId")
     Optional<Lesson> findLessonById(@Param(value = "lessonId") Long lessonId);
 
-    List<LessonInfoToCreateDashboard> findAllLessonInfoByAcademyId(Long academyId);
-
+    @Query("select l.id as id, l.subject as subject from Lesson as l where l.academy.id =:academyId")
+    List<LessonInfoToCreateDashboard> findAllLessonInfoByAcademyId(@Param("academyId") Long academyId);
 
     @Query("SELECT ls FROM Lesson AS ls "
             + "JOIN FETCH ls.academy aca "
