3ba76c74e0de265e3fcd9b7719b3fa73d9649844
byeolhaha <j35635aa@gmail.com>
2023-11-28 02:09:51 +0900

    Style : 응답 변수명 수정

--- src/main/java/org/guzzing/studayserver/domain/academy/controller/dto/response/AcademyGetResponse.java
+++ src/main/java/org/guzzing/studayserver/domain/academy/controller/dto/response/AcademyGetResponse.java
@@ -11,7 +11,7 @@ public record AcademyGetResponse(
         String shuttleAvailability,
         Long expectedFee,
         String updatedDate,
-        List<String> areaOfExpertise,
+        List<String> categories,
         LessonGetResponses lessonGetResponses,
         ReviewPercentGetResponse reviewPercentGetResponse,
         boolean isLiked
