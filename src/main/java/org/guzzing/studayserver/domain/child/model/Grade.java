766f6b12a6b22c869b4fd5145603a8d4fb9890e6
SeYoE <onetuks@naver.com>
2023-11-27 15:37:37 +0900

    Refactor: 학년 level 필드 추가

--- src/main/java/org/guzzing/studayserver/domain/child/model/Grade.java
+++ src/main/java/org/guzzing/studayserver/domain/child/model/Grade.java
@@ -5,32 +5,34 @@ import lombok.Getter;
 
 @Getter
 public enum Grade {
-    ELEMENTARY_SCHOOL_1("초등학교 1학년"),
-    ELEMENTARY_SCHOOL_2("초등학교 2학년"),
-    ELEMENTARY_SCHOOL_3("초등학교 3학년"),
-    ELEMENTARY_SCHOOL_4("초등학교 4학년"),
-    ELEMENTARY_SCHOOL_5("초등학교 5학년"),
-    ELEMENTARY_SCHOOL_6("초등학교 6학년"),
-    MIDDLE_SCHOOL_1("중학교 1학년"),
-    MIDDLE_SCHOOL_2("중학교 2학년"),
-    MIDDLE_SCHOOL_3("중학교 3학년"),
-    HIGH_SCHOOL_1("고등학교 1학년"),
-    HIGH_SCHOOL_2("고등학교 2학년"),
-    HIGH_SCHOOL_3("고등학교 3학년"),
-    GRADUATED("성인");
+    ELEMENTARY_SCHOOL_1(0, "초등학교 1학년"),
+    ELEMENTARY_SCHOOL_2(1, "초등학교 2학년"),
+    ELEMENTARY_SCHOOL_3(2, "초등학교 3학년"),
+    ELEMENTARY_SCHOOL_4(3, "초등학교 4학년"),
+    ELEMENTARY_SCHOOL_5(4, "초등학교 5학년"),
+    ELEMENTARY_SCHOOL_6(5, "초등학교 6학년"),
+    MIDDLE_SCHOOL_1(6, "중학교 1학년"),
+    MIDDLE_SCHOOL_2(7, "중학교 2학년"),
+    MIDDLE_SCHOOL_3(8, "중학교 3학년"),
+    HIGH_SCHOOL_1(9, "고등학교 1학년"),
+    HIGH_SCHOOL_2(10, "고등학교 2학년"),
+    HIGH_SCHOOL_3(11, "고등학교 3학년"),
+    GRADUATED(12, "성인");
 
+    private final int gradeLevel;
     private final String description;
 
-    Grade(String description) {
+    Grade(int gradeLevel, String description) {
+        this.gradeLevel = gradeLevel;
         this.description = description;
     }
 
     public static Grade increaseGrade(final Grade grade) {
-        int ordinal = grade.ordinal();
+        int level = grade.getGradeLevel();
 
-        return ++ ordinal >= Grade.values().length
+        return ++ level >= Grade.values().length
                 ? GRADUATED
-                : Grade.values()[ordinal];
+                : Grade.values()[level];
     }
 
     public static Grade fromDescription(String description) {
