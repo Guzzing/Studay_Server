d6a3dbd69f1b6b1b5c529ed26227f441e6c1bf22
byeolhaha <j35635aa@gmail.com>
2023-11-29 01:58:37 +0900

    Style : 대시보드 아이디 필드 삭제

--- src/main/java/org/guzzing/studayserver/domain/calendar/controller/dto/request/AcademyCalendarDeleteRequest.java
+++ src/main/java/org/guzzing/studayserver/domain/calendar/controller/dto/request/AcademyCalendarDeleteRequest.java
@@ -3,15 +3,13 @@ package org.guzzing.studayserver.domain.calendar.controller.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDate;
+
 import org.guzzing.studayserver.domain.calendar.controller.dto.request.validation.ValidDateString;
 import org.guzzing.studayserver.domain.calendar.service.dto.param.AcademyCalendarDeleteParam;
 
 public record AcademyCalendarDeleteRequest(
-        @Positive
-        @NotNull
-        Long dashboardId,
-
         @Positive
         @NotNull
         Long academyScheduleId,
@@ -26,7 +24,6 @@ public record AcademyCalendarDeleteRequest(
 
     public static AcademyCalendarDeleteParam to(AcademyCalendarDeleteRequest academyCalendarDeleteRequest) {
         return new AcademyCalendarDeleteParam(
-                academyCalendarDeleteRequest.dashboardId,
                 academyCalendarDeleteRequest.academyScheduleId(),
                 academyCalendarDeleteRequest.isAllDeleted(),
                 LocalDate.parse(academyCalendarDeleteRequest.requestDate)
