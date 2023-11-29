d20695ab3a0da47379eb660d20870a85c473cd5a
byeolhaha <j35635aa@gmail.com>
2023-11-29 13:47:07 +0900

    Style : 삭제 요청 요청 api 변경으로 인한 dto 수정

--- src/main/java/org/guzzing/studayserver/domain/calendar/controller/dto/request/AcademyCalendarDeleteRequest.java
+++ src/main/java/org/guzzing/studayserver/domain/calendar/controller/dto/request/AcademyCalendarDeleteRequest.java
@@ -1,26 +1,15 @@
 package org.guzzing.studayserver.domain.calendar.controller.dto.request;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-
-import java.time.LocalDate;
-
 import org.guzzing.studayserver.domain.calendar.service.dto.param.AcademyCalendarDeleteParam;
 
 public record AcademyCalendarDeleteRequest(
-        @Positive
-        @NotNull
         Long academyScheduleId,
-
-        @NotNull
         Boolean isAllDeleted
-
 ) {
-
-    public static AcademyCalendarDeleteParam to(AcademyCalendarDeleteRequest academyCalendarDeleteRequest) {
+    public static AcademyCalendarDeleteParam to(Long academyScheduleId, boolean isAllDeleted) {
         return new AcademyCalendarDeleteParam(
-                academyCalendarDeleteRequest.academyScheduleId(),
-                academyCalendarDeleteRequest.isAllDeleted()
+                academyScheduleId,
+                isAllDeleted
         );
     }
 
