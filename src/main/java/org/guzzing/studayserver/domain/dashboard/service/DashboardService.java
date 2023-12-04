f5967395155d50aeac935d208da51d3ae763ad25
SeYoE <onetuks@naver.com>
2023-12-04 16:43:03 +0900

    Refactor: 대시보드 활성화 시 스케줄 중복 체크

--- src/main/java/org/guzzing/studayserver/domain/dashboard/service/DashboardService.java
+++ src/main/java/org/guzzing/studayserver/domain/dashboard/service/DashboardService.java
@@ -1,9 +1,14 @@
 package org.guzzing.studayserver.domain.dashboard.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.guzzing.studayserver.domain.dashboard.model.Dashboard;
+import org.guzzing.studayserver.domain.dashboard.model.DashboardSchedule;
 import org.guzzing.studayserver.domain.dashboard.model.vo.FeeInfo;
 import org.guzzing.studayserver.domain.dashboard.repository.DashboardRepository;
 import org.guzzing.studayserver.domain.dashboard.repository.DashboardScheduleJpaRepository;
@@ -11,6 +16,7 @@ import org.guzzing.studayserver.domain.dashboard.service.converter.DashboardServ
 import org.guzzing.studayserver.domain.dashboard.service.dto.request.DashboardPostParam;
 import org.guzzing.studayserver.domain.dashboard.service.dto.request.DashboardPutParam;
 import org.guzzing.studayserver.domain.dashboard.service.dto.response.DashboardResult;
+import org.guzzing.studayserver.domain.dashboard.service.overlap.DashboardScheduleOverlapChecker;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,15 +27,17 @@ public class DashboardService {
     private final DashboardServiceConverter serviceConverter;
     private final DashboardRepository dashboardRepository;
     private final DashboardScheduleJpaRepository dashboardScheduleJpaRepository;
+    private final DashboardScheduleOverlapChecker dashboardScheduleOverlapChecker;
 
     public DashboardService(
             final DashboardServiceConverter serviceConverter,
             final DashboardRepository dashboardRepository,
-            final DashboardScheduleJpaRepository dashboardScheduleJpaRepository
-    ) {
+            final DashboardScheduleJpaRepository dashboardScheduleJpaRepository,
+            final DashboardScheduleOverlapChecker dashboardScheduleOverlapChecker) {
         this.serviceConverter = serviceConverter;
         this.dashboardRepository = dashboardRepository;
         this.dashboardScheduleJpaRepository = dashboardScheduleJpaRepository;
+        this.dashboardScheduleOverlapChecker = dashboardScheduleOverlapChecker;
     }
 
     @Transactional
@@ -66,8 +74,20 @@ public class DashboardService {
 
     @Transactional
     public DashboardResult toggleDashboardActiveness(final long dashboardId) {
-        final Dashboard dashboard = dashboardRepository.findDashboardById(dashboardId)
-                .toggleActive();
+        final Dashboard dashboard = dashboardRepository.findDashboardById(dashboardId);
+
+        final List<DashboardSchedule> childDashboardSchedules = dashboardRepository.findActiveOnlyByChildId(
+                        dashboard.getChildId())
+                .stream()
+                .filter(childDashboard -> !Objects.equals(dashboard.getId(), childDashboard.getId()))
+                .map(Dashboard::getDashboardSchedules)
+                .flatMap(List::stream)
+                .collect(Collectors.toCollection(() -> Collections.synchronizedList(new ArrayList<>())));
+
+        dashboardScheduleOverlapChecker.checkScheduleOverlap(dashboard.getDashboardSchedules(),
+                childDashboardSchedules);
+
+        dashboard.toggleActive();
 
         return serviceConverter.from(dashboard);
     }
