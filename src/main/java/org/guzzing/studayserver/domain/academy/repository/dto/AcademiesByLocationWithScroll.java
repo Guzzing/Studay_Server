1bbecb49be2c0d54a01d67a8d4e0ca8adf13b1d8
byeolhaha <j35635aa@gmail.com>
2023-12-09 01:36:12 +0900

    Refactor : no offset 변경으로 인한 레파지토리 dto 변경

--- src/main/java/org/guzzing/studayserver/domain/academy/repository/dto/AcademiesByLocationWithScroll.java
+++ src/main/java/org/guzzing/studayserver/domain/academy/repository/dto/AcademiesByLocationWithScroll.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 public record AcademiesByLocationWithScroll(
         List<AcademyByLocationWithScroll> academiesByLocation,
+        Long beforeLastId,
         boolean hasNext
 ) {
     public static AcademiesByLocationWithScroll of(
             List<AcademyByLocationWithScroll> academiesByLocation,
+            Long beforeLastId,
             boolean hasNext
     ) {
         return new AcademiesByLocationWithScroll(
                 academiesByLocation,
+                beforeLastId,
                 hasNext
         );
     }
