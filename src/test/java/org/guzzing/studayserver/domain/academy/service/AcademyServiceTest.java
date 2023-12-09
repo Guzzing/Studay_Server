88fa9414a5169bd65f45699c3eb6aa4c0d7f93ec
byeolhaha <j35635aa@gmail.com>
2023-12-09 13:45:47 +0900

    Style : 불필요한 코드 제거

--- src/test/java/org/guzzing/studayserver/domain/academy/service/AcademyServiceTest.java
+++ src/test/java/org/guzzing/studayserver/domain/academy/service/AcademyServiceTest.java
@@ -27,13 +27,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Import(TestDatabaseConfig.class)
 @SpringBootTest(webEnvironment = NONE)
-@ActiveProfiles({"dev","oauth"})
 class AcademyServiceTest {
 
     private static final String ACADEMY_NAME_FOR_SEARCH = "코딩";
