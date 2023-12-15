83971462bc85f03e7c7f7c3c635fc2c41fdc57fa
byeolhaha <j35635aa@gmail.com>
2023-12-15 14:54:25 +0900

    Refactor : Mvc bean 설정 어노테이션 추가

--- src/main/java/org/guzzing/studayserver/global/config/WebConfig.java
+++ src/main/java/org/guzzing/studayserver/global/config/WebConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
