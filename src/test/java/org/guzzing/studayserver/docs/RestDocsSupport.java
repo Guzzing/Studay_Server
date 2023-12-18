f2c1bd37c575538934b46eedc2172eac9be83cac
wonu606 <wonu606@gmail.com>
2023-12-18 19:34:37 +0900

    test: AcademyCalendarControllerTest RestDocsSupport 상속받아 테스트 수행하도록 수정

--- src/test/java/org/guzzing/studayserver/docs/RestDocsSupport.java
+++ src/test/java/org/guzzing/studayserver/docs/RestDocsSupport.java
@@ -1,10 +1,16 @@
 package org.guzzing.studayserver.docs;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.guzzing.studayserver.domain.calendar.facade.AcademyCalendarFacade;
+import org.guzzing.studayserver.domain.calendar.service.AcademyCalendarService;
+import org.guzzing.studayserver.domain.child.service.ChildFacade;
+import org.guzzing.studayserver.domain.child.service.ChildService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
