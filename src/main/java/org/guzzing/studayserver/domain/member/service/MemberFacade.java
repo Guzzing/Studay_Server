16eb69be5e3a52989948c9ff2f454663ae152dc0
SeYoE <onetuks@naver.com>
2024-01-03 16:55:14 +0900

    Refactor: 객체 그래프 탐색 방식으로 변경

--- src/main/java/org/guzzing/studayserver/domain/member/service/MemberFacade.java
+++ src/main/java/org/guzzing/studayserver/domain/member/service/MemberFacade.java
@@ -2,8 +2,7 @@ package org.guzzing.studayserver.domain.member.service;
 
 import java.util.List;
 import org.guzzing.studayserver.domain.calendar.service.AcademyCalendarService;
-import org.guzzing.studayserver.domain.child.service.ChildService;
-import org.guzzing.studayserver.domain.child.service.result.ChildrenFindResult.ChildFindResult;
+import org.guzzing.studayserver.domain.child.model.Child;
 import org.guzzing.studayserver.domain.dashboard.service.DashboardService;
 import org.guzzing.studayserver.domain.member.model.Member;
 import org.springframework.stereotype.Component;
@@ -13,18 +12,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberFacade {
 
     private final MemberService memberService;
-    private final ChildService childService;
     private final AcademyCalendarService calendarService;
     private final DashboardService dashboardService;
 
     public MemberFacade(
             final MemberService memberService,
-            final ChildService childService,
             final AcademyCalendarService calendarService,
             final DashboardService dashboardService
     ) {
         this.memberService = memberService;
-        this.childService = childService;
         this.calendarService = calendarService;
         this.dashboardService = dashboardService;
     }
@@ -32,11 +28,9 @@ public class MemberFacade {
     @Transactional
     public void removeMember(final Long memberId) {
         final Member member = memberService.getMember(memberId);
-
-        final List<Long> childIds = childService.findByMemberId(member.getId())
-                .children()
+        final List<Long> childIds = member.getChildren()
                 .stream()
-                .map(ChildFindResult::childId)
+                .map(Child::getId)
                 .toList();
 
         calendarService.removeCalendar(childIds);
