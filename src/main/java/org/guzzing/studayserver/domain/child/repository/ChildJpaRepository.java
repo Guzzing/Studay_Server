96a50c5799f27ee98c11055d050a23c379d5b388
wonu606 <wonu606@gmail.com>
2024-01-11 03:40:58 +0900

    refactor: ChildRepository 구조 변경

--- src/main/java/org/guzzing/studayserver/domain/child/repository/ChildJpaRepository.java
+++ src/main/java/org/guzzing/studayserver/domain/child/repository/ChildJpaRepository.java
@@ -1,20 +1,12 @@
 package org.guzzing.studayserver.domain.child.repository;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.guzzing.studayserver.domain.child.model.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChildRepository {
+public interface ChildJpaRepository extends JpaRepository<Child, Long> {
 
     Optional<Child> findByIdAndMemberId(Long childId, Long memberId);
 
     void deleteByMemberId(final long memberId);
-
-    Child save(Child child);
-
-    Optional<Child> findById(Long childId);
-
-    List<Child> findAll();
 }
