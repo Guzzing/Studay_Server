fe0303463daa7c2835bb2f94736d08f8f47401d7
byeolhaha <j35635aa@gmail.com>
2024-02-01 01:13:57 +0900

    feat : 해당 요일을 포함하는 타임탬플릿 쿼리 오버로딩 메서드로 구현

--- src/main/java/org/guzzing/studayserver/domain/calendar/repository/academytimetemplate/AcademyTimeTemplateJpaRepository.java
+++ src/main/java/org/guzzing/studayserver/domain/calendar/repository/academytimetemplate/AcademyTimeTemplateJpaRepository.java
@@ -1,17 +1,33 @@
 package org.guzzing.studayserver.domain.calendar.repository.academytimetemplate;
 
+import java.time.DayOfWeek;
 import java.util.List;
 import org.guzzing.studayserver.domain.calendar.model.AcademyTimeTemplate;
 import org.guzzing.studayserver.domain.calendar.repository.dto.AcademyTimeTemplateDateInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface AcademyTimeTemplateJpaRepository extends JpaRepository<AcademyTimeTemplate, Long>,
         AcademyTimeTemplateRepository {
 
     AcademyTimeTemplate save(AcademyTimeTemplate academyTimeTemplate);
+
+    @Query(
+        """
+            select att
+            from AcademyTimeTemplate as att
+            where att.dashboardId =:dashboardId
+            and att.dayOfWeek =:dayOfWeek
+            """
+    )
+    List<AcademyTimeTemplateDateInfo> findAcademyTimeTemplateByDashboardId(
+        @Param("dashboardId") Long dashboardId,
+        @Param("dayOfWeek") DayOfWeek dayOfWeek);
+
 
     List<AcademyTimeTemplateDateInfo> findAcademyTimeTemplateByDashboardId(Long dashboardId);
 
