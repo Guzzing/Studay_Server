176e20847cbb9b1b0d214e4adb6d5cfe56d3d743
byeolhaha <j35635aa@gmail.com>
2024-02-16 12:55:44 +0900

    refactor : 지역 이름 얻기 분리로 인한 컨트롤러 수정

--- src/main/java/org/guzzing/studayserver/domain/academy/controller/dto/request/AcademyByLocationWithScrollRequest.java
+++ src/main/java/org/guzzing/studayserver/domain/academy/controller/dto/request/AcademyByLocationWithScrollRequest.java
@@ -3,28 +3,27 @@ package org.guzzing.studayserver.domain.academy.controller.dto.request;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
-import org.guzzing.studayserver.domain.academy.facade.dto.AcademiesByLocationWithScrollFacadeParam;
+import org.guzzing.studayserver.domain.academy.service.dto.param.AcademiesByLocationWithScrollParam;
 
 public record AcademyByLocationWithScrollRequest(
-        @NotNull(message = "Latitude cannot be null")
-        @DecimalMin(value = "-90", message = "Invalid latitude")
-        Double lat,
+    @NotNull(message = "Latitude cannot be null")
+    @DecimalMin(value = "-90", message = "Invalid latitude")
+    Double lat,
 
-        @NotNull(message = "Longitude cannot be null")
-        @DecimalMin(value = "-180", message = "Invalid longitude")
-        Double lng,
+    @NotNull(message = "Longitude cannot be null")
+    @DecimalMin(value = "-180", message = "Invalid longitude")
+    Double lng,
 
-        @PositiveOrZero
-        int pageNumber
+    @PositiveOrZero
+    int pageNumber
 ) {
 
-    public static AcademiesByLocationWithScrollFacadeParam to(AcademyByLocationWithScrollRequest request,
-            Long memberId) {
-        return new AcademiesByLocationWithScrollFacadeParam(
-                request.lat,
-                request.lng,
-                memberId,
-                request.pageNumber
+    public AcademiesByLocationWithScrollParam to(Long memberId) {
+        return new AcademiesByLocationWithScrollParam(
+            lat,
+            lng,
+            memberId,
+            pageNumber
         );
     }
 }
