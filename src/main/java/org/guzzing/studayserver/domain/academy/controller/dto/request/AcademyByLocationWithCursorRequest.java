4a016427af1f361179685d487b7ab1221c060bee
byeolhaha <j35635aa@gmail.com>
2024-02-17 15:06:26 +0900

    feat : 커서 기반 학원 목록 조회 컨트롤러 구현

---
+++ src/main/java/org/guzzing/studayserver/domain/academy/controller/dto/request/AcademyByLocationWithCursorRequest.java
@@ -0,0 +1,31 @@
+package org.guzzing.studayserver.domain.academy.controller.dto.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import org.guzzing.studayserver.domain.academy.service.dto.param.AcademyByLocationWithCursorParam;
+import org.guzzing.studayserver.domain.academy.util.Latitude;
+import org.guzzing.studayserver.domain.academy.util.Longitude;
+
+public record AcademyByLocationWithCursorRequest(
+    @NotNull(message = "Latitude cannot be null")
+    @DecimalMin(value = "-90", message = "Invalid latitude")
+    Double lat,
+
+    @NotNull(message = "Longitude cannot be null")
+    @DecimalMin(value = "-180", message = "Invalid longitude")
+    Double lng,
+
+    @PositiveOrZero(message = "학원 아이디는 음수일 수 없습니다.")
+    Long lastAcademyId
+) {
+
+    public AcademyByLocationWithCursorParam to(Long memberId) {
+        return new AcademyByLocationWithCursorParam(
+            Latitude.of(lat),
+            Longitude.of(lng),
+            memberId,
+            lastAcademyId
+        );
+    }
+}
