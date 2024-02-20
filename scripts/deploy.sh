4d73d71709c582f4dace58996670922bbdad943e
wonu606 <133995055+wonu606@users.noreply.github.com>
2024-02-21 05:27:09 +0900

    chore: codedeploy에게 프로세스 종료 권한 부여

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ CURRENT_PID=$(pgrep -f $JAR_PATH)
 # 실행 중인 애플리케이션이 있으면 종료
 if [[ -n $CURRENT_PID ]]; then
   echo "종료할 프로세스 PID: $CURRENT_PID"
-  kill $CURRENT_PID
+  sudo kill $CURRENT_PID
   
   # 프로세스가 종료될 때까지 대기
   while ps -p $CURRENT_PID &>/dev/null; do
