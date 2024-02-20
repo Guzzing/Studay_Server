f775ea51c8adffd1b42828b5fdc866836429b5ee
wonu606 <133995055+wonu606@users.noreply.github.com>
2024-02-21 05:12:34 +0900

    chore: ec2에서 스크립트를 제대로 수행하지 못하는 문제 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
 
-# 기본 설정: 리포지토리와 애플리케이션 이름
-REPOSITORY="/home/ubuntu/studay"
-APP_NAME="studay"
+# JAR 파일 경로 설정
+JAR_PATH="/home/ubuntu/studay/build/libs/studayserver-0.0.1-SNAPSHOT.jar"
 
-# 최신 JAR 파일 경로 확인
-cd "$REPOSITORY"
-LATEST_JAR=$(ls ./build/libs/*SNAPSHOT.jar | tail -n 1)
+# 실행 중인 애플리케이션의 PID 찾기
+CURRENT_PID=$(pgrep -f $JAR_PATH)
 
-# 실행 중인 애플리케이션 종료 로직
-CURRENT_PID=$(pgrep -f "$LATEST_JAR")
+# 실행 중인 애플리케이션이 있으면 종료
 if [[ -n $CURRENT_PID ]]; then
   echo "종료할 프로세스 PID: $CURRENT_PID"
   kill $CURRENT_PID
-  # 프로세스 종료 대기
-  while ps -p $CURRENT_PID &>/dev/null; do 
+  
+  # 프로세스가 종료될 때까지 대기
+  while ps -p $CURRENT_PID &>/dev/null; do
     sleep 1
   done
 fi
 
-# 심볼릭 링크 업데이트
-SYMLINK_PATH="/home/ubuntu/studay/$APP_NAME-latest.jar"
-ln -sf "$LATEST_JAR" "$SYMLINK_PATH"
-
-# 애플리케이션 백그라운드 실행
-nohup java -jar "$SYMLINK_PATH" &>/dev/null &
+# 애플리케이션 실행
+nohup java -jar $JAR_PATH &>/dev/null &
